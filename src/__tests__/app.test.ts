9aeae2c332eb3826ad0853929f2e3e12842e38f5
andrey nascimento <andrey.nascimento@ciadoleite.com.br>
2023-06-23 02:57:02 -0300

    test: render app.vue

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,11 @@
+import AppVue from "@/App.vue";
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+
+
+describe('App component', () => {
+    it('should render App component', () => {
+        const wrapper = mount(AppVue);
+        expect(wrapper.findComponent(AppVue).exists()).toBeTruthy();
+    });
+})
