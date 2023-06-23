0e4a210af6e9580580985a5794eae5603a22c507
andrey nascimento <andrey.nascimento@ciadoleite.com.br>
2023-06-23 17:36:40 -0300

    chore: added dependecies to coverage test

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 export default defineConfig({
     plugins: [vue()],
     test: {
+        coverage: {
+            provider: 'istanbul'
+        },
         environment: 'happy-dom'
     },
     resolve: {
